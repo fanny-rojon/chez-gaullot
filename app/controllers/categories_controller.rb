2bdecb022dd82b770c1f621d88c496c3a14ea9df
Fanny Rojon <fanny.rojon22@gmail.com>
2020-04-19 19:24:01 +0200

    views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,44 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit]
+    skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @categories = Category.order(:name)
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    # no need for app/views/categorys/update.html.erb
+    redirect_to category_path(@category)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :photo)
+  end
 end
