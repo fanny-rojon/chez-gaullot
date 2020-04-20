72313eaf70a4104b560944204cbafaa4db26ead4
Fanny Rojon <fanny.rojon22@gmail.com>
2020-04-20 17:14:53 +0200

    prefilled form with current user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all Users"
+
+User.destroy_all
+
+puts "Creating Users..."
+
+catherine = User.create!(email: "catherine@email.com", password: "varennes", first_name: "Catherine");
+caroline = User.create!(email: "caroline@email.com", password: "varennes", first_name: "Caroline");
+laure = User.create!(email: "laure@email.com", password: "varennes", first_name: "Laure");
+delphine = User.create!(email: "delphine@email.com", password: "varennes", first_name: "Delphine");
+celine = User.create!(email: "celine@email.com", password: "varennes", first_name: "Céline");
+fanny = User.create!(email: "fanny@email.com", password: "varennes", first_name: "Fanny");
+martine = User.create!(email: "martine@email.com", password: "varennes", first_name: "Martine");
+cecile = User.create!(email: "cecile@email.com", password: "varennes", first_name: "Cécile");
+
+
+puts "Created #{User.count} user(s)"
