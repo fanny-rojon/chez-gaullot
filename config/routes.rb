c8ac7f5089d4c081079f1cd9fb8b3961ba61b3d0
Fanny Rojon <fanny.rojon22@gmail.com>
2020-04-24 14:37:58 +0200

    expressions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'expressions', to: 'pages#expressions', as: :expressions
   resources :categories, only: %i[index show new create edit update]
   resources :recipes, only: %i[show new create edit update]
 end
